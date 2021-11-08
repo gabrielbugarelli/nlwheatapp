45088a308e452c9e3406c59c917186411a29c3b9
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-11-08 11:36:39 -0400

    chore: configuração base e fonts

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E5E7EB'
+  },
+
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#374151'
+  }
+})
