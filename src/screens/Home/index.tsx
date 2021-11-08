b1295ca00ee6d3d791efc186d7dc8fbceee4ad81
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-11-08 13:30:07 -0400

    chore: tipando extenções .svg e .png

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,12 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View } from "react-native"
+import { Header } from "../../components/Header";
 import { styles } from "./styles"
 
 export const Home = () => {
   return (
     <View style={ styles.container }>
-      <Text style={styles.text}> Página Home </Text>
+      <Header />
     </View>
   )
 }
