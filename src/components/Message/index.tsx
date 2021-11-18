457d4165344fdb530c3293e83849dd6630d4bf27
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-11-18 13:01:04 -0400

    feat: implentação da animação no message

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View  } from 'react-native';
 import { UserPhoto } from '../UserPhoto';
+import { MotiView } from 'moti';
 
 import { styles } from './styles';
 
@@ -19,7 +20,12 @@ type Props = {
 
 export const Message = ({ data }: Props) => {
   return (
-    <View style={styles.container}>
+    <MotiView 
+      from={{ opacity: 0, translateY: -100}}
+      animate={{opacity: 1, translateY: 0}}
+      transition={{ type: 'timing', duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>
         {data.text}
       </Text>
@@ -34,6 +40,6 @@ export const Message = ({ data }: Props) => {
           {data.user.name}
         </Text>
       </View>
-    </View>
+    </MotiView>
   )
 }
